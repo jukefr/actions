ac91aae50c45cbc6c1d73815bbf1b27240f2f1a5
Kevin Jullien <hi@juke.fr>
2019-01-11 14:49:49 +0100

    git works

--- git/entrypoint.sh
+++ git/entrypoint.sh
@@ -4,7 +4,6 @@
 set -e
 
 # Create ~/.netrc to skip Github login
-echo "Creating ~/.netrc"
 echo "machine github.com" > ~/.netrc
 echo "login $USER" >> ~/.netrc
 echo "password $GITHUB_TOKEN" >> ~/.netrc
@@ -12,6 +11,5 @@ chmod 600 ~/.netrc
 
 # All commands are in /bin
 # We copy /bin to /urs/local/bin on the container
-echo "Executing git $*"
 sh -c "$*"
 
