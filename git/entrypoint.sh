4adcde16159f823088aabdc665ae74de7d8f2502
Kevin Jullien <hi@juke.fr>
2019-01-11 15:25:33 +0100

    fix git config

--- git/entrypoint.sh
+++ git/entrypoint.sh
@@ -9,6 +9,10 @@ echo "login $USER" >> ~/.netrc
 echo "password $GITHUB_TOKEN" >> ~/.netrc
 chmod 600 ~/.netrc
 
+# Configure Git
+git config --global user.email "actions@github.com"
+git config --global user.name "Github Actions"
+
 # All commands are in /bin
 # We copy /bin to /urs/local/bin on the container
 sh -c "git $*"
